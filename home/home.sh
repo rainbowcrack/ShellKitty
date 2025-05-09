04b6bc6c44535235ecddf46710fcd9048cba0320
Izabel Chaves <145824500+rainbowcrack@users.noreply.github.com>
2025-05-09 08:57:19 -0300

    Create home.sh

---
+++ home/home.sh
@@ -0,0 +1,24 @@
+!#/bin/bash 
+
+echo "
+/\_/\
+( o.o)
+ > ^ < ........... WELCOME TO SHELLKITTY ><)))º>
+ 
+// construcao (dockerfile)
+// instalacao (BD e git)
+// ambiente (security e IA)
+// QA (testes de versao)
+
+Options:
+[0] Exit
+[1] Menu
+[2] BD Postgre and JSON --> dar sudo su e -y para todas as permissoes
+[3] Maven and Spark
+[4] Docker --> CRUD de Docker e perguntar imagem
+[5] Docker building --> construção
+[6] IA --> Python, PyTorch, TensorTable
+[7] Security
+[8] QA
+
+@ Developed by Rainbowcrack"
